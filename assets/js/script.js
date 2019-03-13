dc509e5ddfb8032b6cd75012a665055ff5a5a228
stremsky <40144110+stremsky@users.noreply.github.com>
2019-03-13 11:41:20 +0300

    1

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,3 +25,32 @@ $(document).keyup(function(e) {
 });
 
 
+
+
+// Кнопка вверх
+
+$(function() {
+ 
+	$(window).scroll(function() {
+	 
+		if($(this).scrollTop() != 0) {
+		 
+		$('.up').fadeIn();
+		 
+		} else {
+		 
+		$('.up').fadeOut();
+		 
+		}
+		 
+		});
+	 
+		$('.up').click(function() {
+	 
+		$('body,html').animate({scrollTop:0},800);
+	 
+	});
+ 
+});
+
+
